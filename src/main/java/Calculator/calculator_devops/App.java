a5ef207c9884f61ee200bbacfea7fe7ce5e54dda
Vivekk <vivekk_selvam@outlook.com>
2020-06-29 09:10:12 +0530

    changes for demo execution

--- src/main/java/Calculator/calculator_devops/App.java
+++ src/main/java/Calculator/calculator_devops/App.java
@@ -1,13 +1,17 @@
 package Calculator.calculator_devops;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+	public int add(int a,int b)
+	{
+		int sum = 0;
+		sum = a + b;
+		return sum;
+	}
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	App a = new App();
+    	int result = a.add(5, 4);
+        System.out.println("The result is "+result);
     }
 }
--- src/test/java/Calculator/calculator_devops/AppTest.java
+++ src/test/java/Calculator/calculator_devops/AppTest.java
@@ -1,38 +1,16 @@
 package Calculator.calculator_devops;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import org.junit.jupiter.api.Test;
+
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	App a = new App();
+    @Test
+   public void testadd()
+   {
+    	assertEquals(6, a.add(3, 3));
+   }
+    
 }
